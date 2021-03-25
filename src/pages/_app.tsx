91ee5f685667274b737f27578132f88d9de2157b
ippee <ippee.music@gmail.com>
2021-03-25 21:56:17 +0900

    add icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import { store } from '../lib/store'
 
 import '../styles/globals.css'
+import '../../public/assets/fontello/css/fontello.css'
 
 function MyApp({ Component, pageProps }) {
   return (
