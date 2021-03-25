d61fbdc7f303071954622b80910470a027d6a27f
ippee <ippee.music@gmail.com>
2021-03-25 13:10:52 +0900

    add a choosing bg function

--- src/components/Popup/BGContent.tsx
+++ src/components/Popup/BGContent.tsx
@@ -2,21 +2,31 @@ import { useAppDispatch } from '../../lib/hooks'
 import { setBG } from '../../lib/newtabSlice'
 import { ContentStyle } from '../Popup'
 
-import styles from '../../styles/Popup.module.css'
+import styles from '../../styles/PopupContent.module.css'
 
 const BGContent: React.FC<ContentStyle> = (props) => {
   const dispatch = useAppDispatch()
-  const updateBG = () => {
-    dispatch(setBG('../assets/img/demo2.jpg'))
-  }
+
+  const sampleImages = new Array(5).fill('').map((_, i) => `./assets/img/demo${i+1}.jpg`)
 
   return (
     <div 
       className={styles.settingsBGContent}
       style={props.style}
     >
-      <p>bg</p>
-      <input type="button" value="update" onClick={updateBG} />
+      <div className={styles.imageGrid}>
+        {
+          sampleImages.map((url, i) => { return (
+            <img
+              className={styles.bgThumbnail}
+              onContextMenu={(e)=> e.preventDefault()}
+              src={url}
+              alt={`image${i+1}`}
+              onClick={(url) => dispatch(setBG(url.currentTarget.src))}
+            />
+          )})
+        }
+      </div>
     </div>
   )
 }
