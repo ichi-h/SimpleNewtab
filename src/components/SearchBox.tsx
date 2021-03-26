9c9723b0d5073e28457301af0faa88481df79cad
ippee <ippee.music@gmail.com>
2021-03-26 20:11:53 +0900

    add a search function

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,9 +1,15 @@
+import { useState } from 'react'
+
 import styles from '../styles/Home.module.css'
 
 const SearchBox: React.FC = () => {
   return (
     <div className={styles.searchBox}>
-      <input type="text"/>
+      <form action="https://duckduckgo.com/" method="get">
+          <input type="hidden" name="hl" />
+          <input type="textbox" name="q" />
+          <input type="submit" name="btnG" value="Search" />
+      </form>
     </div>
   )
 }
