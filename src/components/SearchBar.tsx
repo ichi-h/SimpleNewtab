285c106939d36c957f4e5a5758c3f3875f95c611
ippee <ippee.music@gmail.com>
2021-03-27 14:49:05 +0900

    design SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,13 @@
+import React from 'react'
+import { useState } from 'react'
 import { useAppSelector } from '../lib/hooks'
 
-import styles from '../styles/Home.module.css'
+import styles from '../styles/SearchBar.module.css'
 
 const SearchBar: React.FC = () => {
   const searchEngine = useAppSelector(state => state.searchEngine)
   const searchBarIsShow = useAppSelector(state => state.searchBarIsShow)
+  const [searchBarVal, setSearchBarVal] = useState('')
 
   let searchAction: string
   switch (searchEngine) {
@@ -19,16 +22,44 @@ const SearchBar: React.FC = () => {
       break
   }
 
+  const updateSearchBar = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchBarVal(e.target.value)
+  }
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    if (searchBarVal === "") e.preventDefault()
+  }
+
   if (!searchBarIsShow) return <></>
 
   return (
-    <div className={styles.searchBar}>
-      <form action={searchAction} method="get">
-          <input type="hidden" name="hl" />
-          <input type="textbox" name="q" />
-          <input type="submit" name="btnG" value="Search" />
-      </form>
-    </div>
+    <form
+      className={styles.searchBar}
+      action={searchAction}
+      method="get"
+      onSubmit={onSubmit}
+    >
+        <input type="hidden" name="hl" />
+        <div className={styles.searchBarItem}>
+          <label htmlFor="search-button">
+            <input
+              className={styles.searchButton}
+              id="search-button"
+              type="submit"
+              name="btnG"
+              value="Search"
+            />
+            <i className="icon-search" />
+          </label>
+          <input
+            className={styles.searchBarTextBox}
+            type="textbox"
+            name="q"
+            defaultValue={searchBarVal}
+            onChange={updateSearchBar}
+          />
+        </div>
+    </form>
   )
 }
 
